d04398d6cc4464fcf4d94e3a902a5c0087ae7805
duoduoxu <duoduoxu@tencent.com>
2025-07-12 22:36:26 +0800

    feat: bundleIdentifier

---
+++ app.config.js
@@ -0,0 +1,35 @@
+const IS_DEV = process.env.APP_VARIANT === "development";
+const IS_PREVIEW = process.env.APP_VARIANT === "preview";
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return "StickerSmash (Dev)";
+  }
+
+  if (IS_PREVIEW) {
+    return "StickerSmash (Preview)";
+  }
+
+  return "StickerSmash: Emoji Stickers";
+};
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return "com.yourname.stickersmash.dev";
+  }
+
+  if (IS_PREVIEW) {
+    return "com.yourname.stickersmash.preview";
+  }
+
+  return "com.yourname.stickersmash";
+};
+
+export default ({ config }) => ({
+  ...config,
+  name: getAppName(),
+  ios: {
+    ...config.ios,
+    bundleIdentifier: getUniqueIdentifier(),
+  },
+});
